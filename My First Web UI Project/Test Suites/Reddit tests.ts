99ee0d69e5ed5a9910748698f5bcef499c3749c8
RefinSoch <30792682+RefinSoch@users.noreply.github.com>
2021-01-28 23:58:07 +0100

    Add files via upload

---
+++ My First Web UI Project/Test Suites/Reddit tests.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Reddit tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f3c649b4-74d4-4137-b761-b56c6a3f54e9</testSuiteGuid>
+   <testCaseLink>
+      <guid>d6e2c61d-45bb-49c5-88a2-018701a0d8de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestRedditFaildLogin</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8444ea93-92ac-4d22-8a2c-53f97f94738d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b80a9a83-03ba-4389-b687-a3634b54e16c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a4fe8cf3-173b-41b8-acb4-eccafcf09501</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
